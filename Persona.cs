c3e28696575851fb683f4f751fd52d16072594ff
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-10-11 14:24:22 -0500

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Personas
+{
+    class Persona
+    {
+        #region Atributos
+        private string nombre;
+        private string telefono;
+        private byte edad;
+        #endregion
+
+        #region Propiedades
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public byte Edad { 
+            get => edad;
+            set
+            {
+                if(  edad < 12  || edad > 80     )
+                {
+                    edad = 12;
+                }
+                else
+                {
+                    edad = value;
+                }
+                
+            }
+        }
+        #endregion
+
+
+        public Persona(string nombre, string telefono, byte edad)
+        {
+            Nombre = nombre;
+            Telefono = telefono;
+            Edad = edad;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Personas;
+namespace Arreglos_Objetos_G4_2022_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona[] integrantesEquipo1 = new Persona[3];
+
+            integrantesEquipo1[0] = new Persona("Armando","5510124798",22);
+            integrantesEquipo1[1] = new Persona("Luis", "55221090", 20);
+            integrantesEquipo1[2] = new Persona("Carlos", "551248988", 21);
+
+            for (int i = 0; i< integrantesEquipo1.Length; i++ )
+            {
+                Console.WriteLine("Nombre: {0} ", integrantesEquipo1[i].Nombre);
+            }
+
+
+            Persona[,] parejasG4 = new Persona[2,2];
+
+            parejasG4[0,0] = new Persona("Armando","5517458878",20);
+            parejasG4[0,1] = new Persona("Juan","5522148878",)
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
